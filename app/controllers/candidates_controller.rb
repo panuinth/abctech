8317b8b0556d62e30b19bf4866c32fd271e65179
Panu Ausavasereelert <panu@panu.in.th>
2013-06-25 16:50:15 +0700

    add error message on upload photo page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -43,13 +43,13 @@ class CandidatesController < ApplicationController
   end
 
   def update_picture
-    @candidate = Candidate.upload_picture(:uuid => params[:uuid], :imageBase64 => params[:imageBase64].tempfile)
+    @candidate = Candidate.upload_picture(:uuid => params[:uuid], :imageBase64 => params[:imageBase64].tempfile) if not params[:imageBase64].blank?
 
     respond_to do |format|
-      if @candidate["status"] == 1
+      if not params[:imageBase64].blank? and @candidate["status"] == 1
         format.html { redirect_to action: "show", notice: "Candidate was successfully updated.", :uuid => params[:uuid] }
       else
-        format.html { render action: "new" }
+        format.html { redirect_to '/candidates/upload_picture', :notice => "Invalid data, please try again." }
       end
     end
 
